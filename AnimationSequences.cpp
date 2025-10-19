f86bf350dcaa588b7ad0bbfcb4030e60b4fa2092
rananna <rnorthca@gmail.com>
2025-10-19 18:11:59 -0400

    Update AnimationSequences.cpp

--- AnimationSequences.cpp
+++ AnimationSequences.cpp
@@ -1374,8 +1374,8 @@ void generatePartyMode(SequencerTrack tracks[3]) {
     // Clear and show a random flicker effect
     s0 = add_step(tracks[0], s0, SEQ_CMD_CLEAR_SEGMENT, 0, -1, 0, 0);
     s2 = add_step(tracks[2], s2, SEQ_CMD_CLEAR_SEGMENT, 2, -1, 0, 0);
-    s0 = add_step(tracks[0], s0, SEQ_CMD_RANDOM_FLICKER_TEXT, 0, -1, 500, 50, solid_block);
-    s2 = add_step(tracks[2], s2, SEQ_CMD_RANDOM_FLICKER_TEXT, 2, -1, 500, 50, solid_block);
+    s0 = add_step(tracks[0], s0, SEQ_CMD_RANDOM_FLICKER_TEXT, 0, -1, 50, 500, solid_block);
+    s2 = add_step(tracks[2], s2, SEQ_CMD_RANDOM_FLICKER_TEXT, 2, -1, 50, 500, solid_block);
 
     // Final wait before loop repeats
     s0 = add_step(tracks[0], s0, SEQ_CMD_WAIT, 0, 0, 500, 0);
