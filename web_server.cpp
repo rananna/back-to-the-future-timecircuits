685faf47ae300e5589a54b4f8089b1debe88682b
rananna <rnorthca@gmail.com>
2025-10-20 14:59:47 -0400

    Update web_server.cpp

--- web_server.cpp
+++ web_server.cpp
@@ -591,7 +591,7 @@ void setupWebRoutes() {
   
   server.on("/api/greatScott", HTTP_POST, [](AsyncWebServerRequest *request){
     if (hardwareInitialized) {
-        playSound("/EASTER_EGG.mp3", false, -1);
+        runBootSequence();
     }
     request->send(200, "text/plain", "Great Scott!");
   });
